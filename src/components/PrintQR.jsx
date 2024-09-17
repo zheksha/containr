603d7135d653017adce21dc5a98da6fd49b893b8
zheksha <zheksha@gmail.com>
2024-09-16 20:58:45 -0500

    fix: :bug: Remove extra slash

--- src/components/PrintQR.jsx
+++ src/components/PrintQR.jsx
@@ -23,7 +23,7 @@ const PrintQR = ({ selectedContainer }) => {
       <QRCode
         size={256}
         style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
-        value={`${appUrl}/view/${id}`}
+        value={`${appUrl}view/${id}`}
         viewBox={`0 0 256 256`}
       />
 
